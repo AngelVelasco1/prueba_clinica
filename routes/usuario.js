a87aea75241b3196b5f1181c891be617f518fdde
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-07-12 18:52:49 -0500

    feat: :sparkles: Add fifth consult references to usuario table

--- routes/usuario.js
+++ routes/usuario.js
@@ -24,11 +24,31 @@ storageUsuario.get('/', proxyUsuario, (req, res) => {
 
 
 storageUsuario.get('/:usu_id', proxyUsuario, (req, res) => {
+    const pacienteId = req.params.usu_id;
     const action = `
     SELECT c.* FROM cita c
-    WHERE c.cit_datosUsuario AND c.cit_estadoCita AND c.cit_fecha >= CURDATE()
+    WHERE c.cit_datosUsuario = ${pacienteId} AND c.cit_estadoCita AND c.cit_fecha >= CURDATE()
     ORDER BY c.cit_fecha
     `;
+    conx.query(
+        action, (err, result) => {
+            if (err) {
+                console.error('Error de conexion:', err.message);
+                res.status(200);
+            } else {
+                res.send(JSON.stringify(result));
+            }
+        })
+})
+
+
+storageUsuario.get('/:med_nroMatriculaProsional', proxyUsuario, (req, res) => {
+    const medicoId = req.params.med_nroMatriculaProsional;
+
+    const action = `SELECT u.* FROM usuario u
+    INNER JOIN cita c ON u.usu_id = c.cit_datosUsuario
+    WHERE c.cit_medico = ${medicoId}
+    ORDER BY c.cit_fecha`;
     conx.query(
         action, (err, result) => {
             if (err) {
