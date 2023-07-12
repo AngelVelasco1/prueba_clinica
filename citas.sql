ea1d12e8bc4b8b57d91ab7446d2da264dbd8bba3
Angel Velasco <angelgg2020@outlook.com>
2023-07-12 09:29:23 -0500

    feat: :sparkles: Insert all data

--- citas.sql
+++ citas.sql
@@ -103,3 +103,72 @@ ALTER TABLE consultorio
 ADD CONSTRAINT fk_cons_codigo
 FOREIGN KEY (cons_codigo)
 REFERENCES medico(med_nroMatriculaProsional);
+
+ALTER TABLE medico MODIFY COLUMN med_nombreCompleto VARCHAR(255);
+INSERT INTO especialidad (esp_id, esp_nombre) VALUES (1, 'Cardiología');
+INSERT INTO especialidad (esp_id, esp_nombre) VALUES (2, 'Dermatología');
+INSERT INTO especialidad (esp_id, esp_nombre) VALUES (3, 'Pediatría');
+INSERT INTO especialidad (esp_id, esp_nombre) VALUES (4, 'Oftalmología');
+INSERT INTO especialidad (esp_id, esp_nombre) VALUES (5, 'Gastroenterología');
+
+
+
+INSERT INTO consultorio (cons_codigo, cons_nombre) VALUES (1, 'Consultorio A');
+INSERT INTO consultorio (cons_codigo, cons_nombre) VALUES (2, 'Consultorio B');
+INSERT INTO consultorio (cons_codigo, cons_nombre) VALUES (3, 'Consultorio C');
+INSERT INTO consultorio (cons_codigo, cons_nombre) VALUES (4, 'Consultorio D');
+INSERT INTO consultorio (cons_codigo, cons_nombre) VALUES (5, 'Consultorio E');
+
+
+
+INSERT INTO medico (med_nroMatriculaProsional, med_nombreCompleto, med_consultorio, med_especialidad) VALUES (123456, 'D.r JuanPérez', 1, 1);
+INSERT INTO medico (med_nroMatriculaProsional, med_nombreCompleto, med_consultorio, med_especialidad) VALUES (234567, 'Dra. Ana Rodríguez', 2, 2);
+INSERT INTO medico (med_nroMatriculaProsional, med_nombreCompleto, med_consultorio, med_especialidad) VALUES (345678, 'Dr. Luis Martínez', 3, 3);
+INSERT INTO medico (med_nroMatriculaProsional, med_nombreCompleto, med_consultorio, med_especialidad) VALUES (456789, 'Dra. Laura Gómez', 4, 4);
+INSERT INTO medico (med_nroMatriculaProsional, med_nombreCompleto, med_consultorio, med_especialidad) VALUES (567890, 'Dr. Carlos Sánchez', 5, 5);
+
+INSERT INTO estado_cita (estcita_id, estcita_nombre) VALUES (1, 'Pendiente');
+INSERT INTO estado_cita (estcita_id, estcita_nombre) VALUES (2, 'Confirmada');
+INSERT INTO estado_cita (estcita_id, estcita_nombre) VALUES (3, 'Cancelada');
+INSERT INTO estado_cita (estcita_id, estcita_nombre) VALUES (4, 'Realizada');
+INSERT INTO estado_cita (estcita_id, estcita_nombre) VALUES (5, 'Ausente');
+
+
+
+INSERT INTO tipo_documento (tipdoc_id, tipdoc_nombre, tipdoc_abreviatura) VALUES (1, 'Cédula de Ciudadanía', 'CC');
+INSERT INTO tipo_documento (tipdoc_id, tipdoc_nombre, tipdoc_abreviatura) VALUES (2, 'Tarjeta de Identidad', 'TI');
+INSERT INTO tipo_documento (tipdoc_id, tipdoc_nombre, tipdoc_abreviatura) VALUES (3, 'Cédula Extranjera', 'CE');
+INSERT INTO tipo_documento (tipdoc_id, tipdoc_nombre, tipdoc_abreviatura) VALUES (4, 'Pasaporte', 'PAS');
+INSERT INTO tipo_documento (tipdoc_id, tipdoc_nombre, tipdoc_abreviatura) VALUES (5, 'Registro Civil', 'RC');
+
+
+
+INSERT INTO genero (gen_id, gen_nombre, gen_abreviatura) VALUES (1, 'Masculino', 'M');
+INSERT INTO genero (gen_id, gen_nombre, gen_abreviatura) VALUES (2, 'Femenino', 'F');
+INSERT INTO genero (gen_id, gen_nombre, gen_abreviatura) VALUES (3, 'Otro', 'O');
+INSERT INTO genero (gen_id, gen_nombre, gen_abreviatura) VALUES (4, 'No especificado', 'NE');
+INSERT INTO genero (gen_id, gen_nombre, gen_abreviatura) VALUES (5, 'Prefiero no decirlo', 'PND');
+
+
+
+INSERT INTO acudiente (acu_codigo, acu_nombreCompleto, acu_telefono, acu_direccion) VALUES (1, 'Luisa Pérez', '1234567890', 'Calle 123, Ciudad');
+INSERT INTO acudiente (acu_codigo, acu_nombreCompleto, acu_telefono, acu_direccion) VALUES (2, 'Pedro Gómez', '9876543210', 'Avenida 456, Ciudad');
+INSERT INTO acudiente (acu_codigo, acu_nombreCompleto, acu_telefono, acu_direccion) VALUES (3, 'María Rodríguez', '5555555555', 'Carrera 789, Ciudad');
+INSERT INTO acudiente (acu_codigo, acu_nombreCompleto, acu_telefono, acu_direccion) VALUES (4, 'Andrés Torres', '9999999999', 'Calle Principal, Ciudad');
+INSERT INTO acudiente (acu_codigo, acu_nombreCompleto, acu_telefono, acu_direccion) VALUES (5, 'Laura Sánchez', '1111111111', 'Avenida Central, Ciudad');
+
+
+
+INSERT INTO usuario (usu_id, usu_nombre, usu_segdo_nombre, usu_primer_apellido_usuar, usu_segdo_apellido_usuar, usu_telefono, usu_direccion, usu_email, usu_tipodoc, usu_genero, usu_acudiente) VALUES (10, 'Juan', 'David', 'Pérez', 'Gómez', '1234567890', 'Calle 123, Ciudad', 'juan@example.com', 1, 1, 1);
+INSERT INTO usuario (usu_id, usu_nombre, usu_segdo_nombre, usu_primer_apellido_usuar, usu_segdo_apellido_usuar, usu_telefono, usu_direccion, usu_email, usu_tipodoc, usu_genero, usu_acudiente) VALUES (11, 'María', 'Fernanda', 'Rodríguez', 'García', '9876543210', 'Avenida 456, Ciudad', 'maria@example.com', 2, 2, 2);
+INSERT INTO usuario (usu_id, usu_nombre, usu_segdo_nombre, usu_primer_apellido_usuar, usu_segdo_apellido_usuar, usu_telefono, usu_direccion, usu_email, usu_tipodoc, usu_genero, usu_acudiente) VALUES (12, 'Carlos', '', 'González', 'Sánchez', '5555555555', 'Carrera 789, Ciudad', 'carlos@example.com', 1, 1, 3);
+INSERT INTO usuario (usu_id, usu_nombre, usu_segdo_nombre, usu_primer_apellido_usuar, usu_segdo_apellido_usuar, usu_telefono, usu_direccion, usu_email, usu_tipodoc, usu_genero, usu_acudiente) VALUES (13, 'Ana', 'María', 'López', 'Torres', '9999999999', 'Calle Principal, Ciudad', 'ana@example.com', 3, 2, 4);
+INSERT INTO usuario (usu_id, usu_nombre, usu_segdo_nombre, usu_primer_apellido_usuar, usu_segdo_apellido_usuar, usu_telefono, usu_direccion, usu_email, usu_tipodoc, usu_genero, usu_acudiente) VALUES (14, 'Pedro', '', 'Ramírez', 'Sánchez', '1111111111', 'Avenida Central, Ciudad', 'pedro@example.com', 4, 3, 5);
+
+
+
+INSERT INTO cita (cit_codigo, cit_fecha, cit_estadoCita, cit_medico, cit_datosUsuario) VALUES (10, '2023-07-13', 2, 123456, 3);
+INSERT INTO cita (cit_codigo, cit_fecha, cit_estadoCita, cit_medico, cit_datosUsuario) VALUES (11, '2023-07-14', 2, 234567, 2);
+INSERT INTO cita (cit_codigo, cit_fecha, cit_estadoCita, cit_medico, cit_datosUsuario) VALUES (12, '2023-07-15', 3, 345678, 3);
+INSERT INTO cita (cit_codigo, cit_fecha, cit_estadoCita, cit_medico, cit_datosUsuario) VALUES (13, '2023-07-16', 4, 456789, 4);
+INSERT INTO cita (cit_codigo, cit_fecha, cit_estadoCita, cit_medico, cit_datosUsuario) VALUES (14, '2023-07-17', 5, 567890, 5);
---
+++ readme.md
@@ -0,0 +1,93 @@
+# Consultorio clinica Exercise
+
+Aspectos esenciales del proyexto.
+
+## Requisitos previos
+
+Asegúrate de tener instalado lo siguiente antes de iniciar el proyecto:
+
+- Node.js: [instalar Node.js](https://nodejs.org)
+- NPM
+
+## Instalación
+
+1. Clona este repositorio en tu máquina local:
+
+   ```shell
+   git clone https://github.com/tu-usuario/nombre-del-proyecto.git
+   ```
+
+2. Navega hasta el directorio del proyecto:
+
+   ```shell
+   cd nombre-del-proyecto
+   ```
+
+3. Instala las dependencias del proyecto:
+
+   ```shell
+   npm install dotenv express mysql2 nodemon -E -D
+   ```
+4. Inicia el servidor mediante el comando `npm run dev`.
+
+
+## Configuración
+
+El proyecto utiliza variables de entorno para la configuración de la conexión a la base de datos.
+
+### MY_CONFIG
+
+La variable de entorno `MY_CONFIG` define la configuración del servidor de la base de datos. Debes proporcionar el hostname y el puerto de conexión. 
+
+```
+MY_CONFIG={"hostname": "localhost", "port": 8050}
+```
+
+### MY_CONNECT
+
+La variable de entorno `MY_CONNECT` define los parámetros de conexión a la base de datos, como el host, usuario, contraseña, base de datos y puerto.
+
+```
+MY_CONNECT={"host": "127.0.0.1", "user": "root", "database": "inventory", "password": "123456", "port": 3306}
+```
+
+## Dependencias
+
+El proyecto utiliza las siguientes dependencias:
+
+- dotenv - 16.3.1
+- express - 4.18.2
+- mysql2 - 3.5.0
+- nodemon - 2.0.22
+- class-transformer: 0.5.1,
+
+
+##  Uso de Thunder Client
+
+Thunder client es la herramienta en la que se realizan las pruebas de conexion:
+
+1. Elije el metodo apropiado para la consulta
+- GET
+- POST
+- UPDATE
+- DELETE
+
+2. Agrega la URL correspondiente:
+```
+http://{hostname}:{port}/nombre_ruta_correspondiente
+```
+
+3. Asegurarse de tener un `content-type: application/json`
+
+4. Si es un envio de datos (POST), agregar el body correspondiente a las columnas requeridas de la tabla
+
+---
+
+## Licencia
+
+Este proyecto está bajo la Licencia ISC.
+
+
+## Contacto
+
+Si tienes alguna pregunta o sugerencia, no dudes en ponerte en contacto conmigo a través de [email](angelgg2020@outlook.com) o [Linkeind](https://www.linkedin.com/in/angel-velascoo/).
