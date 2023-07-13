b076535cf62f51efc410cd9d6f8d7b503475a525
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-07-12 19:59:12 -0500

    feat: :sparkles: Add eleventh consult references to cita table

--- routes/cita.js
+++ routes/cita.js
@@ -35,5 +35,22 @@ storageCita.get('/:cit_fecha', proxyCita, (req, res) => {
         })
 })
 
+storageCita.get('/:usu_genero', proxyCita, (req, res) => {
+    const generoUsuario = req.params.usu_genero;
+    const action = `SELECT c.* FROM cita c 
+                    INNER JOIN usuario u ON c.cit_datosUsuario = u.usu_id
+                    INNER JOIN estado_cita ec ON c.cit_estadoCita = ec.estcita_id
+                     WHERE u.usu_genero = ${generoUsuario} AND ec.estcita_id = 2`;
+    conx.query(
+        action, (err, result)  => {
+            if (err) {
+                console.error('Error de conexion:', err.message);
+                res.status(200);
+            } else {
+                res.send(JSON.stringify(result));
+            }
+        })
+})
+
 
 export default storageCita;
